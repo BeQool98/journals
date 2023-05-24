9f2b68d12d0253950cb6daaea747779dd6d229ce
BeQool98 <davidistifanus82@gmail.com>
2023-05-24 13:16:48 -0700

    adding a login and about html file

--- academic_journals/urls.py
+++ academic_journals/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('academic_journals_app.urls'))
+    path('', include('academic_journals_app.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
